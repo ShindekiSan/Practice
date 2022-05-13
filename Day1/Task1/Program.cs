c5e725f87bcbb3856dfa37d3bcff908121c759ec
ShindekiSan <abelchuk.maksim@gmail.com>
2022-05-13 20:50:28 +0300

    day1 commit

---
+++ Day1/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿double area;
+int radius;
+
+try
+{
+    Console.WriteLine("Введите целочисленный неотрицательный радиус:");
+    radius = int.Parse(Console.ReadLine());
+
+    if (radius > 0)
+    {
+        area = Math.PI * Math.Pow(radius, 2);
+        Console.WriteLine(area);
+    }
+    else
+    {
+        Console.WriteLine("Радиус не может быть отрицательным");
+    }
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
