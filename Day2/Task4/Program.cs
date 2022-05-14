8d0057f8cc0e135ec2e6247e9f4a9bf519e2a69e
ShindekiSan <abelchuk.maksim@gmail.com>
2022-05-14 17:34:01 +0300

    added task 4 for day 2

--- Day2/Task4/Program.cs
+++ Day2/Task4/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var result = 0;
+int i = 0;
+int j = 0;
+int k = 0;
+
+Console.WriteLine("Цикл For");
+for (i = 0; i <= 101; i++) 
+{ 
+    if (i % 2 != 0)
+    {
+        Console.WriteLine(i);
+    }
+}
+
+Console.WriteLine("\nЦикл while");
+while (j <= 101)
+{
+    if (j % 2 != 0)
+    {
+        Console.WriteLine(j);
+    }
+    j++;
+}
+
+Console.WriteLine("\nЦикл do while");
+do
+{
+    if (k % 2 != 0)
+    {
+        Console.WriteLine(k);
+    }
+    k++;
+}
+while (k <= 101);
